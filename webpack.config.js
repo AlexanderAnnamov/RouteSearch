e7bfd1bea5a2896513290337fac974361ea811a5
Александр Аннамов <hash_nam@mail.ru>
2024-11-24 12:42:16 +0500

    init app

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const createExpoWebpackConfigAsync = require("@expo/webpack-config");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  if (env.mode === "development") {
+    config.plugins.push(
+      new ReactRefreshWebpackPlugin({ disableRefreshCheck: true })
+    );
+  }
+
+  return config;
+};
